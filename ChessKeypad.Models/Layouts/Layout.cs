19083926640457355e77ce82a6784501c09fb114
Tom Binnington <tom@binnington.net>
2021-05-27 07:34:05 +0100

    Added comments

--- ChessKeypad.Models/Layouts/Layout.cs
+++ ChessKeypad.Models/Layouts/Layout.cs
@@ -7,6 +7,13 @@ namespace ChessKeypad.Models.Layouts
     {
         public abstract Dictionary<Coordinate, Cell> Cells { get; }
 
+        /// <summary>
+        /// Returns whether the given coordinate is valid.
+        /// If <paramref name="position"/> == null then the method should check whether the coordinate is valid for any position
+        /// </summary>
+        /// <param name="coordinate">The coordinate to test</param>
+        /// <param name="position">The poisition in the number being generated, if required</param>
+        /// <returns>Whether the coordinate is valid</returns>
         public virtual bool IsCoordinateValid(Coordinate coordinate, int? position = null)
         {
             return Cells.ContainsKey(coordinate) && Cells[coordinate].IsValid(position);
--- ChessKeypad.Models/Moves/Move.cs
+++ ChessKeypad.Models/Moves/Move.cs
@@ -2,6 +2,11 @@
 {
     public abstract record Move (bool VariableLength)
     {
+        /// <summary>
+        /// Process a single iteration of this move
+        /// </summary>
+        /// <param name="start">The coordinate to start from</param>
+        /// <returns>The resulting coordinate</returns>
         public abstract Coordinate Process(Coordinate start);
     }
 }
